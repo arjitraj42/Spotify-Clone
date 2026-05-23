1207abb08dc4359e828f8ed755d70559dc5f48f6
Arjit Raj <arjitraj00@gmail.com>
2026-05-24 03:42:24 +0530

    Deployment Fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const cookieparser = require("cookie-parser")
+const cors = require("cors")
 const authRoutes = require("./routes/auth.routes")
 const musicRoutes = require("./routes/music.routes")
 const userRoutes = require("./routes/user.routes")
 const playlistRoutes = require("./routes/playlist.routes")
 
 const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://spotify-clone-dr6c.vercel.app'],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieparser());
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://spotify-clone-mz14.onrender.com';
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
