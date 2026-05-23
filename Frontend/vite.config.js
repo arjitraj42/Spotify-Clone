7b600ae99be58ae0c5073bd54c6c4d2ede0e6aa7
Arjit Raj <arjitraj00@gmail.com>
2026-05-24 03:38:28 +0530

    Deployment Fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://spotify-clone-mz14.onrender.com',
         changeOrigin: true,
       }
     }
