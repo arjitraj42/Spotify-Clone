bfaa547f547cfe2d1ae6dfd0c91e22535ffa8497
Arjit Raj <arjitraj00@gmail.com>
2026-05-24 03:46:15 +0530

    add vercel proxy rewrites

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import axios from 'axios'
-
-axios.defaults.baseURL = 'https://spotify-clone-mz14.onrender.com';
-axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
