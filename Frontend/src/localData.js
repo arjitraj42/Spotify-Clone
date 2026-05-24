f5acbb3316db4f7ca30cfb74cb7df0d41c4e4a37
Arjit Raj <arjitraj00@gmail.com>
2026-05-24 13:05:34 +0530

    Added remaining songs to frontend home page

--- Frontend/src/localData.js
+++ Frontend/src/localData.js
@@ -14,6 +14,30 @@ export const localMusic = [
     albumId: 'local_album_1'
   },
   {
+    _id: 'local_song_8',
+    title: 'Jaan Ban Gaye',
+    artist: { username: 'Jubin Nautiyal' },
+    coverUrl: 'https://picsum.photos/seed/saiyaara/300/300',
+    uri: '/Jaan Ban Gaye - Khuda Haafiz (320 kbps).mp3',
+    albumId: 'local_album_2'
+  },
+  {
+    _id: 'local_song_12',
+    title: 'Sitaare',
+    artist: { username: 'Sunidhi Chauhan' },
+    coverUrl: 'https://picsum.photos/seed/aiyaary/300/300',
+    uri: '/Sitaare (From &quot;Ikkis&quot;) - Ikkis (320 kbps).mp3',
+    albumId: 'local_album_3'
+  },
+  {
+    _id: 'local_song_5',
+    title: 'Dhurandhar Title Track',
+    artist: { username: 'Arijit Singh' },
+    coverUrl: 'https://picsum.photos/seed/kedarnath/300/300',
+    uri: '/Dhurandhar - Title Track - Dhurandhar (320 kbps).mp3',
+    albumId: 'local_album_1'
+  },
+  {
     _id: 'local_song_2',
     title: 'Barbaad Song',
     artist: { username: 'Jubin Nautiyal' },
@@ -22,11 +46,59 @@ export const localMusic = [
     albumId: 'local_album_2'
   },
   {
+    _id: 'local_song_6',
+    title: 'Gehra Hua',
+    artist: { username: 'Sunidhi Chauhan' },
+    coverUrl: 'https://picsum.photos/seed/aiyaary/300/300',
+    uri: '/Gehra Hua - Dhurandhar (320 kbps).mp3',
+    albumId: 'local_album_3'
+  },
+  {
+    _id: 'local_song_10',
+    title: 'Lutt Le Gaya',
+    artist: { username: 'Arijit Singh' },
+    coverUrl: 'https://picsum.photos/seed/kedarnath/300/300',
+    uri: '/Lutt Le Gaya - Dhurandhar (320 kbps).mp3',
+    albumId: 'local_album_1'
+  },
+  {
+    _id: 'local_song_11',
+    title: 'Move',
+    artist: { username: 'Jubin Nautiyal' },
+    coverUrl: 'https://picsum.photos/seed/saiyaara/300/300',
+    uri: '/Move - Yeh Ishq Ishq _ 8K Video _ Dhuarandhar _ Ranveer Singh _ Shashwat Sachdev _ Sonu Nigam _Reble.mp3',
+    albumId: 'local_album_2'
+  },
+  {
     _id: 'local_song_3',
     title: 'Lae Dooba',
     artist: { username: 'Sunidhi Chauhan' },
     coverUrl: 'https://picsum.photos/seed/aiyaary/300/300',
     uri: '/Lae Dooba - Full Video _ Aiyaary _ Sidharth Malhotra, Rakul Preet _ Sunidhi Chauhan _ Rochak Kohli.mp3',
+    albumId: 'local_album_3'
+  },
+  {
+    _id: 'local_song_7',
+    title: 'Ishq Jalakar',
+    artist: { username: 'Arijit Singh' },
+    coverUrl: 'https://picsum.photos/seed/kedarnath/300/300',
+    uri: '/Ishq Jalakar - Karvaan - Dhurandhar (320 kbps).mp3',
+    albumId: 'local_album_1'
+  },
+  {
+    _id: 'local_song_9',
+    title: 'Khat',
+    artist: { username: 'Jubin Nautiyal' },
+    coverUrl: 'https://picsum.photos/seed/saiyaara/300/300',
+    uri: '/Khat - Khat (320 kbps).mp3',
+    albumId: 'local_album_2'
+  },
+  {
+    _id: 'local_song_4',
+    title: 'Sweetheart',
+    artist: { username: 'Sunidhi Chauhan' },
+    coverUrl: 'https://picsum.photos/seed/aiyaary/300/300',
+    uri: '/Sweetheart - Kedarnath (320 kbps).mp3',
     albumId: 'local_album_3'
   }
 ];
